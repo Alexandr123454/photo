5ca4df34ad89b61e8ead729099fd915942895556
alexandr <alexandrov6927@gmail.com>
2019-09-27 23:43:56 +0300

    'app'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { loadimages } from './API_DATA'
-import { Spring, Transition, animated } from 'react-spring/renderprops';
+import { Spring } from 'react-spring/renderprops';
+import classnames from 'classnames';
 import './App.css';
 
 const getData = state => ({
@@ -17,20 +18,28 @@ const getMethods = dispatch => ({
 
 class App extends React.Component {
 
-async componentDidMount () {
-  let response = await loadimages()
+  state = {
+    toggle: true,
+  }
 
-  let images = [...response.hits]
+  async componentDidMount () {
+    let response = await loadimages()
+    let images = [...response.hits]
+    this.props.setData(images) 
+  }
 
-  this.props.setData(images) // 
-}
+  handleClick = () => {
+    this.setState(state => ({
+      toggle: !state.toggle,
+    }))
+  }
 
   render() {
 
-    const { images } = this.props 
+    const { images } = this.props
+    const { toggle } = this.state 
 
     return (
-      
       <Spring
         from={{ marginTop: -5000 }}
         to={{ marginTop: 0 }}
@@ -38,13 +47,29 @@ async componentDidMount () {
       >
       {props => (
         <div style={props}>
-          <div className="images">
+          <div className="button-flex">
+            <button 
+              className="button" 
+              onClick={() => {this.handleClick()}}
+            >Change view
+            </button>
+          </div>
+          <div
+            className={classnames({
+              'gridImages': toggle === true,
+              'simpleView': toggle === false,
+            })}
+          >
             {images.length 
               ? images.map(item => (
                   <img  
                     src={item.largeImageURL}
                     alt={item.tags}
-                    className="images__item"
+                    className="gridImages__item"
+                    className={classnames({
+                      'gridImages__item': toggle === true,
+                      'simpleView__item': toggle === false,
+                    })}
                   />
                 )) 
               : "No images"}
